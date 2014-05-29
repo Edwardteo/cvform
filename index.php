146c0b7b0786af439d0f3277938792e81f7e4942
Juan C Teofilo <jcteo87@gmail.com>
2014-05-28 23:20:30 -0500

    add sec and acciones

--- index.php
+++ index.php
@@ -1,6 +1,19 @@
 <link rel="stylesheet" href="css.css">
 <?php
+$sec="portada";
+if(isset($_GET['sec'])){
+	$sec=$_GET['sec'];
+}
+if(!empty($_POST)){
+	include "acciones.php";
+}
 include "header.html";
-include "portada.html";
+if(file_exists($sec.".html")){
+	include "portada.html";
+}elseif(file_exists($sec.".php")){
+	include "portada.php";
+}else{
+	echo "<div class='msg404'>404 Not Found</div>";
+}
 include "footer.html";
 ?>
