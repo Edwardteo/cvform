de959826f9a7773da1c4b1d21f604d38a497b12c
Eduardo Teofilo <eteo92@gmail.com>
2014-05-29 01:57:45 -0500

    nuevo dump

--- DBcurriculum.sql
+++ DBcurriculum.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 29-05-2014 a las 06:43:39
+-- Tiempo de generación: 29-05-2014 a las 06:55:11
 -- Versión del servidor: 5.1.36
 -- Versión de PHP: 5.3.0
 
@@ -31,5 +31,92 @@ CREATE TABLE IF NOT EXISTS `escolaridad` (
 
 --
 -- Volcar la base de datos para la tabla `escolaridad`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `experiencia`
+--
+
+CREATE TABLE IF NOT EXISTS `experiencia` (
+  `id_experiencia` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `id_personales` mediumint(8) unsigned NOT NULL,
+  `nombre_emresa` varchar(255) DEFAULT NULL,
+  `fecha_inicio` date DEFAULT NULL,
+  `fecha_fin` date DEFAULT NULL,
+  `puesto` varchar(100) DEFAULT NULL,
+  `funciones` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id_experiencia`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Volcar la base de datos para la tabla `experiencia`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `herramientas`
+--
+
+CREATE TABLE IF NOT EXISTS `herramientas` (
+  `id_herramientas` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `id_personales` mediumint(8) unsigned NOT NULL,
+  `nombre_heramienta` varchar(50) DEFAULT NULL,
+  `descripcion` varchar(255) DEFAULT NULL,
+  `dominio` varchar(3) DEFAULT NULL,
+  PRIMARY KEY (`id_herramientas`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Volcar la base de datos para la tabla `herramientas`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `idiomas`
+--
+
+CREATE TABLE IF NOT EXISTS `idiomas` (
+  `id_idiomas` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `id_personales` mediumint(8) unsigned NOT NULL,
+  `idioma` varchar(50) DEFAULT NULL,
+  `dominio` varchar(3) DEFAULT NULL,
+  `certificado` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id_idiomas`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Volcar la base de datos para la tabla `idiomas`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `personales`
+--
+
+CREATE TABLE IF NOT EXISTS `personales` (
+  `id_personales` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) DEFAULT NULL,
+  `apaterno` varchar(50) DEFAULT NULL,
+  `amaterno` varchar(50) DEFAULT NULL,
+  `fecha_nacimiento` date DEFAULT NULL,
+  `nacionalidad` varchar(50) DEFAULT NULL,
+  `estado_civil` varchar(50) DEFAULT NULL,
+  `codigo_postal` varchar(10) DEFAULT NULL,
+  `telefono` varchar(20) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id_personales`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Volcar la base de datos para la tabla `personales`
 --
 
